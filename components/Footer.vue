276ac7d04afbaab876bc2a55168b4bb8951d1830
Dedy ibrahim <dedyibrahym23@gmail.com>
2025-12-25 19:48:05 +0700

    first Commit Company Profile

---
+++ components/Footer.vue
@@ -0,0 +1,115 @@
+<template>
+  <div>
+    <footer class="site-footer">
+      <div class="site-footer__upper">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-lg-6">
+              <div class="footer-widget footer-widget__about">
+                <a href="/" class="logo">
+                  <img src="/assets/images/logo-1-2.png" width="136" alt="">
+                </a>
+                <p>Solusi Digital Terpercaya untuk Bisnis Anda</p>
+                <a href="#" class="thm-btn"><span>Download App</span></a><!-- /.thm-btn -->
+              </div><!-- /.footer-widget footer-widget__about -->
+            </div><!-- /.col-lg-4 -->
+            <div class="col-xl-2 col-lg-6">
+              <div class="footer-widget footer-widget__links">
+                <h3 class="footer-widget__title">Explore</h3><!-- /.footer-widget__title -->
+                <ul class="list-unstyled footer-widget__links-list">
+                  <li><a href="#">About</a></li>
+                  <li><a href="#">Our Team</a></li>
+                  <li><a href="#">Contact</a></li>
+                  <li><a href="#">Services</a></li>
+                </ul><!-- /.list-unstyled footer-widget__links-list -->
+              </div><!-- /.footer-widget footer-widget__links -->
+            </div><!-- /.col-lg-2 -->
+            <div class="col-xl-2 col-lg-6">
+              <div class="footer-widget footer-widget__contact">
+                <h3 class="footer-widget__title">Contact</h3><!-- /.footer-widget__title -->
+                <ul class="footer-widget__contact-list list-unstyled">
+                  <li>
+                    <i class="fa fa-phone-square"></i>
+                    <a href="tel:777-888-0000">+62 819-3496-6774</a>
+                  </li>
+                  <li>
+                    <i class="fa fa-envelope"></i>
+                    <a href="mailto:needhelp@oapee.com">admin@safaqita.com</a>
+                  </li>
+                  <li>
+                    <i class="fa fa-map-marker"></i>
+                   Poja, Desa Candi, Kecamatan Dungkek, <br> Kabupaten Sumenep, Jawa Timur 69474
+                  </li>
+                </ul><!-- /.footer-widget__contact-list list-unstyled -->
+              </div><!-- /.footer-widget footer-widget__contact -->
+            </div><!-- /.col-lg-2 -->
+            <div class="col-xl-4 col-lg-6">
+              <div class="footer-widget footer-widget__newsletter">
+                <h3 class="footer-widget__title">Newsletter</h3>
+                <form action="#" class="footer-widget__newsletter-form">
+                  <input type="text" placeholder="Email address" name="email">
+                  <button type="submit"><i class="fa fa-envelope"></i></button>
+                </form><!-- /.footer-widget__newsletter-form -->
+                <p>Sign up for our latest news & articles. We won’t give you spam mails.</p>
+              </div><!-- /.footer-widget footer-widget__newsletter -->
+            </div><!-- /.col-lg-4 -->
+          </div><!-- /.row -->
+        </div><!-- /.container -->
+      </div><!-- /.site-footer__upper -->
+      <div class="site-footer__bottom">
+        <div class="container">
+          <div class="inner-container">
+            <p>© copyright 2025 by PT Safaqita Digital Solution</p>
+            <div class="footer-social">
+              <a href="#"><i class="fab fa-facebook-square"></i></a>
+              <a href="#"><i class="fab fa-twitter"></i></a>
+              <a href="#"><i class="fab fa-instagram"></i></a>
+              <a href="#"><i class="fab fa-pinterest-p"></i></a>
+            </div><!-- /.footer-social -->
+          </div><!-- /.inner-container -->
+        </div><!-- /.container -->
+      </div><!-- /.site-footer__bottom -->
+    </footer>
+
+      <a @click="scrollTop" class="scroll-to-target scroll-to-top" :style="`display: ${scrollBtn ? 'inline' : 'none'}`" href="#"><i class="fa fa-angle-up"></i></a>
+
+
+  </div>
+</template>
+
+<script>
+    export default {
+      name: "Footer",
+      data(){
+        return {
+          scrollBtn: false
+        }
+      },
+      mounted() {
+        window.addEventListener('scroll', this.handleScroll);
+      },
+
+      methods: {
+
+        handleScroll () {
+
+          if (window.scrollY > 70) {
+            this.scrollBtn = true
+          }
+          else if (window.scrollY < 70) {
+            this.scrollBtn = false
+          }
+        },
+
+        scrollTop () {
+          window.scrollTo(0, 0);
+        }
+
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/Service.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="service-one" id="features">
+    <div class="container">
+      <div class="block-title text-center">
+        <h3>Safaqita Memberikan Solusi <span>Internet untuk Anda </span></h3>
+        <p>Anda dapat menjadi bagian dari bisnis ini sebagai Mitra, dan Agen.</p>
+      </div><!-- /.block-title -->
+     <div class="row">
+  <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="0ms">
+    <div class="service-one__single">
+      <h3>Internet Cepat <br> & Stabil</h3>
+      <p>Koneksi aman dan lancar untuk rumah, bisnis, dan aktivitas digital setiap hari.</p>
+      <div class="service-one__icon">
+        <i class="oapee-icon-laptop"></i>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="100ms">
+    <div class="service-one__single">
+      <h3>Gabung Jadi <br> Mitra Safaqita</h3>
+      <p>Mulai usaha ISP di wilayah Anda dengan dukungan penuh dan sistem terpercaya.</p>
+      <div class="service-one__icon">
+        <i class="oapee-icon-presentation"></i>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="200ms">
+    <div class="service-one__single">
+      <h3>Referral & <br> Komisi Menarik</h3>
+      <p>Dapatkan keuntungan dari sistem referral dan promosi yang kami sediakan gratis.</p>
+      <div class="service-one__icon">
+        <i class="oapee-icon-target"></i>
+      </div>
+    </div>
+  </div>
+  <div class="col-xl-3 col-lg-6 col-md-6 col-sm-12 wow fadeInUp" data-wow-duration="1500ms" data-wow-delay="300ms">
+    <div class="service-one__single">
+      <h3>Dukungan <br> Promosi Digital</h3>
+      <p>Mitra mendapat materi promosi, desain, dan konten branding siap pakai & gratis.</p>
+      <div class="service-one__icon">
+        <i class="oapee-icon-visualization"></i>
+      </div>
+    </div>
+  </div>
+</div>
+
+    </div><!-- /.container -->
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "Service"
+    }
+</script>
+
+<style scoped>
+
+</style>
